a9aeb080881e05321a793657f67b889a805cdf45
franz <francis.lalaguna@gmail.com>
2023-06-17 12:21:26 +0800

    update home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import Bloglist from "./Bloglist";
 import useFetch from '../customHooks/useFetch'
 const Home = () => {
     //fires a function every render
-    const {data:blogs, setData, isPending , error} = useFetch('http://localhost:8000/blogs')
+    const {data:blogs, isPending , error} = useFetch('http://localhost:8000/blogs')
     // const handleDelete = (id) =>{
     //     console.log('http://localhost:8000/blogs/'+ id)
     //     fetch('http://localhost:8000/blogs/'+ id,{
@@ -15,7 +15,7 @@ const Home = () => {
         <div className="home">
             {error &&<div>{error}</div> }
             {isPending && <div>Fetching Data...</div>}
-           {blogs && <Bloglist blogs={blogs} setData={setData}/>} 
+           {blogs && <Bloglist blogs={blogs}/>} 
         </div>
 
      );
